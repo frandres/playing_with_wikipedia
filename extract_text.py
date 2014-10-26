bba5ead67a2dc161443bb59b47a2dbc94054a81f
Francisco Rodriguez Drumond <frandres@gmail.com>
2014-10-26 21:33:24 +0100

    now working for petrach (basic demo)

--- extract_text.py
+++ extract_text.py
@@ -1,8 +1,15 @@
 import re
 
-def extract_text(id0, id1, text, window):
-    pattern = re.compile('.*('+ id0+'.*'+id1+ ').*')
-    groups = pattern.match(text).groups()
+def extract_text(id0, id1, text, window=0):
+    id0 = id0.replace('_',' ')
+    id1 = id1.replace('_',' ')
+    regexp = '.*('+id0+'.*?'+id1+').*'
+    print(regexp)
+    pattern = re.compile(regexp,re.DOTALL)
+    m = pattern.match(text)
+    if m is None or len(m.groups()) ==0:
+        return 'Pattern not found'
+    groups = m.groups()
     # We work with the first occurence of the id0+ id1 relation.
     # We assume that it is the most relevant one.
     t = groups[0]
