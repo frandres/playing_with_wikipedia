8be3a06f8fac8973163e1e37ea3d735b448b8745
Francisco Rodriguez Drumond <frandres@gmail.com>
2014-11-09 23:03:16 +0100

    Now we can take whole sentence, and we have an option not to eliminate the ids

--- extract_text.py
+++ extract_text.py
@@ -1,10 +1,13 @@
 import re
 
-def extract_text(id0, id1, text, window=0, both_ways = False):
+def extract_text(id0, id1, text, window=0, both_ways = False, whole_sentence = True,eliminate_ids = False):
     id0 = id0.replace('_',' ')
     id0 = 'Canzoniere'
     id1 = id1.replace('_',' ')
     regexp = '(.*)('+id0+'.*?'+id1+').*'
+    if whole_sentence:
+        # regexp = '(.*)\.(.*?'+id0+'.*?'+id1+'.*?\.).*'
+        window = 0
     #regexp = '.*?('+id0+'^((?!'+id0+').)*$'+id1+')(.*)'
     print ('Using a window of size ' + str(window) + ' and the regexp:') 
     print(regexp)
@@ -29,14 +32,28 @@ def extract_text(id0, id1, text, window=0, both_ways = False):
 
     for i in range(0,len(matches)):
         
-        # Augment the obtained text with a window of size window
-        matches[i] = text[positions[i]-window:positions[i]+len(matches[i])+window]
-        # Replace the ids.
+        # Augment the obtained text with a window of size window or with a whole sentence.
 
-        matches[i] = matches[i].replace(id0,'')
-        matches[i] = matches[i].replace(id1,'')
+        i0 = positions[i]-window
+        i1 = positions[i]+len(matches[i])+window
+
+        if whole_sentence:
+            while i0>=0 and text[i0-1] != '.':
+                i0-= 1
+            while i1<len(text) and text[i1] != '.':
+                i1+= 1
+
+        matches[i] = text[i0:i1]
+        # Replace the ids.
+        if eliminate_ids:
+            matches[i] = matches[i].replace(id0,'')
+            matches[i] = matches[i].replace(id1,'')
+        matches[i] = matches[i].strip()
  
+    for x in matches:
+        print x
+        print '--------------'
     if both_ways:
-        return matches + extract_text(id1, id0, text, window)
+        return matches + extract_text(id1, id0, text, window=0, both_ways = both_ways, whole_sentence = whole_sentence,eliminate_ids = eliminate_ids)
     else:
         return matches
