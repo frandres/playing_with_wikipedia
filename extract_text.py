5e349760083d58032cbf97a4e5c2669ebea9818d
Francisco Rodriguez Drumond <frandres@gmail.com>
2014-11-09 21:57:03 +0100

    We now take more than 1 string and we can return the strings in both directions of the relation

--- extract_text.py
+++ extract_text.py
@@ -1,28 +1,42 @@
 import re
 
-def extract_text(id0, id1, text, window=0):
+def extract_text(id0, id1, text, window=0, both_ways = False):
     id0 = id0.replace('_',' ')
+    id0 = 'Canzoniere'
     id1 = id1.replace('_',' ')
-    regexp = '.*('+id0+'.*?'+id1+').*'
+    regexp = '(.*)('+id0+'.*?'+id1+').*'
+    #regexp = '.*?('+id0+'^((?!'+id0+').)*$'+id1+')(.*)'
+    print ('Using a window of size ' + str(window) + ' and the regexp:') 
     print(regexp)
+    
     pattern = re.compile(regexp,re.DOTALL)
-    m = pattern.match(text)
-    if m is None or len(m.groups()) ==0:
-        return 'Pattern not found'
-    groups = m.groups()
-    # We work with the first occurence of the id0+ id1 relation.
-    # We assume that it is the most relevant one.
-    t = groups[0]
 
-    '''
-    The following lines of code augment the obtained text with a window of size window_size
-    Note that this is an exploratory version: a more optimal version would extract this
-    in the regular expressions.
-    '''
-    lt = len(t)
-    for i in range(0,len(text)-lt):
-        if text[i:i+lt] == t:
-            t = text[max(0,i-window):min(len(text),i+lt+window)]
-    t = t.replace(id0,'')
-    t = t.replace(id1,'')
-    return t
+    keep_looking = True
+    matches = []
+    text_for_reg_exps = text
+
+    positions = []
+    while keep_looking:
+        m = pattern.search(text_for_reg_exps)
+        if m is None:
+            break
+        groups = m.groups()
+        keep_looking = len(groups) == 2
+        match = groups[1]
+        text_for_reg_exps = groups[0]
+        positions.append(len(text_for_reg_exps))
+        matches.append(match)
+
+    for i in range(0,len(matches)):
+        
+        # Augment the obtained text with a window of size window
+        matches[i] = text[positions[i]-window:positions[i]+len(matches[i])+window]
+        # Replace the ids.
+
+        matches[i] = matches[i].replace(id0,'')
+        matches[i] = matches[i].replace(id1,'')
+ 
+    if both_ways:
+        return matches + extract_text(id1, id0, text, window)
+    else:
+        return matches
