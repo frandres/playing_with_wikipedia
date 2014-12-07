d8a8c55a6a50e62643c7064f55695ece461f4129
Francisco Rodriguez Drumond <frandres@gmail.com>
2014-12-07 16:06:42 +0100

    Now counting the words in-between the contexts

--- longest_subsequence.py
+++ longest_subsequence.py
@@ -10,9 +10,46 @@ def findLCS(matrix,x,y,i,j):
     else:
         return findLCS(matrix,x,y,i-1,j)
 
+
+def count_words_inbetween(s,lss):
+    s_count = [0 for x in range(0,len(lss)+1)]
+
+    lss_i = 0
+    s_i = 0
+    while(lss[lss_i]!=s[s_i]):
+        s_i+=1
+
+    s_count[0] = s_i
+
+    lss_i+=1
+
+
+    while(lss_i<len(lss)):
+        tmp = s_i
+
+        while(s_i<len(s) and lss[lss_i]!=s[s_i]):
+            s_i+=1
+        s_count[lss_i] = s_i-tmp-1
+        lss_i+=1
+
+    s_count[len(lss)] = len(s)-s_i-1
+
+    return s_count
+
 def get_longest_subsequence(s1,s2):
     s1 = s1.split(' ')
+    tmp = []
+    for w in s1:
+        if w != '':
+            tmp.append(w.strip())
+    s1 = tmp
+
     s2 = s2.split(' ')
+    tmp = []
+    for w in s2:
+        if w != '':
+            tmp.append(w.strip())
+    s2 = tmp
 
     m = len(s1)
     n = len(s2)
@@ -30,5 +67,25 @@ def get_longest_subsequence(s1,s2):
 
     res = findLCS(matrix,s1,s2,m,n)
     res = res.strip()
-    return res
+
+    if res == '':
+        return None
+
+    res_words = res.split(' ')
+
+    # Now let's find the number of words in the original strings between each of the words
+    # of the longest common subsequence    
+
+    s1_count = count_words_inbetween(s1,res_words)
+    s2_count = count_words_inbetween(s2,res_words)
+
+    '''
+    words = res.split(' ') 
+    res = []
+    for word in words:
+        if word != '':
+            res.append(word)
+    res = ' '.join(res)
+    '''
+    return ((res,zip(s1_count,s2_count)))
 
--- ranking.py
+++ ranking.py
@@ -19,28 +19,63 @@ def is_valid_context(text):
         has_verb = has_verb or pos[0] == 'V'
         has_noun = has_noun or pos[0:2] == 'NN'
 
-    return has_verb and has_noun and valid
-def process_contexts(contexts):
+    return has_verb or has_noun #and valid
+
+# Given a list of counts of words, computes some relevats statistics and returns this in a string representation
+def process_word_counts(word_counts):
+#    return [' 'for counts in word_counts]
+    return ['(' + str(min(counts))+' '+ str(max(counts))+')' for counts in word_counts]
+
+'''
+    Given a list of contexts, processes them by extracting all the pairwise longest subsequences, keeping a count of the words (TODO: Explain this better), and then building a frequency table.
+'''
+def process_contexts(contexts,minimum_freq):
+    lss = {}
     for i in range(0,len(contexts)):
         for j in range(i,len(contexts)):
-            t = get_longest_subsequence(contexts[i],contexts[j])
-            words = t.split(' ')
-            if len(words)>3:
-                yield t
+            glss = get_longest_subsequence(contexts[i],contexts[j])
+            if glss is not None:
+                t= glss[0]
+                word_counts = glss[1]
+                words = t.split(' ')
+                if len(words)>3:
+                    if t in lss.keys():
+                       for i in range(0,len(word_counts)):
+                            lss[t][i].append(word_counts[i][0])
+                            lss[t][i].append(word_counts[i][1])
+                    else: 
+                        lss[t]= []
+                        for i in range(0,len(word_counts)):
+                            lss[t].append([word_counts[i][0],word_counts[i][1]])
+    i = 0
+    processed_contexts = {}
+    for (expression,word_counts) in lss.items():
+        freq = len(word_counts[0])/2
+        if freq>=minimum_freq and is_valid_context(expression):
+            words = expression.split(' ')
+            context =''
+            processed_word_counts = process_word_counts(word_counts)
+            for i in range(0,len(words)):
+                context+=processed_word_counts[i]+words[i]
+            context+=processed_word_counts[len(processed_word_counts)-1]
+            processed_contexts[context]=freq
 
-def build_frequency_table(contexts,minimum_freq):
+    return processed_contexts
+
+def build_ranking(contexts,minimum_freq):
     frequency = {}
 
+    frequency = process_contexts(contexts,minimum_freq)
+    '''
     for t in process_contexts(contexts): 
-        if t in frequency:
+        if expression in frequency:
             frequency[t]+=1
         else:
             frequency[t] = 1.0
-
+    '''
     sorted_text = []
     for (text,freq) in frequency.items():
-        if freq>=minimum_freq and is_valid_context(text):
-            sorted_text.append((freq,text))
+        sorted_text.append((freq,text))
 
     sorted_text.sort(reverse= True)
 
@@ -71,9 +106,9 @@ def get_ranking(triplets, minimum_freq = 1,contexts=None):
         for i in range(0,len(contexts)):
             contexts[i] = preprocess_context(contexts[i])
 
-    sorted_text = build_frequency_table(contexts,minimum_freq)
+    sorted_text = build_ranking(contexts,minimum_freq)
 
     print '-------------- RANKING ----------------'
-    for i in range(0,min(50000,len(sorted_text))):
+    for i in range(0,min(100,len(sorted_text))):
         print str(i) + ' |' + sorted_text[i][1].encode('utf8') + '| with freq: '+ str(sorted_text[i][0])
         print ' '
